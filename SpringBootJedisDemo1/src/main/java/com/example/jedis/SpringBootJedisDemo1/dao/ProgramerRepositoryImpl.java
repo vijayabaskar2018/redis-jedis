f55a2793cb115b18b9c400ccccd19075b2c88b61
vijayabaskar2018 <vviji.1988@gmail.com>
2021-06-08 13:17:44 +0530

    added list, set and hash functionality to Redis-Jedis Demo Apllication

--- SpringBootJedisDemo1/src/main/java/com/example/jedis/SpringBootJedisDemo1/dao/ProgramerRepositoryImpl.java
+++ SpringBootJedisDemo1/src/main/java/com/example/jedis/SpringBootJedisDemo1/dao/ProgramerRepositoryImpl.java
@@ -1,14 +1,23 @@
 package com.example.jedis.SpringBootJedisDemo1.dao;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.example.jedis.SpringBootJedisDemo1.model.Programmer;
+
 
 @Repository
 public class ProgramerRepositoryImpl implements ProgrammerRepository {
+	
+	public static final String REDIS_LIST_KEY = "ProgrammerList";
+	public static final String REDIS_SET_KEY = "ProgrammerSet";
+	public static final String REDIS_HASH_KEY = "ProgrammerHash";
 
 	@Autowired
 	private RedisTemplate<String,Object> redisTemplate;
@@ -24,5 +33,64 @@ public class ProgramerRepositoryImpl implements ProgrammerRepository {
 	public String getProgrammerAsString(String idKey) {
 		return (String) redisTemplate.opsForValue().get(idKey);
 	}
+
+	@Override
+	public void addToProgrammerList(Programmer programmer) {
+		redisTemplate.opsForList().leftPush(REDIS_LIST_KEY, programmer);
+		
+	}
+
+	@Override
+	public List<Programmer> getProgrammerListMembers() {
+		return (List<Programmer>) (List<?>) redisTemplate.opsForList().range(REDIS_LIST_KEY, 0, -1);
+	}
+
+	@Override
+	public Long getProgrammersListCount() {
+		return redisTemplate.opsForList().size(REDIS_LIST_KEY);
+	}
+
+	@Override
+	public void AddProgrammerToSet(Programmer... programmer) {
+		redisTemplate.opsForSet().add(REDIS_SET_KEY, programmer);
+	}
+
+	@Override
+	public Set<Programmer> getProgrammersSet() {
+		return (Set<Programmer>) (Set<?>) redisTemplate.opsForSet().members(REDIS_SET_KEY);
+	}
+
+	@Override
+	public boolean isSetMember(Programmer programmer) {
+		return redisTemplate.opsForSet().isMember(REDIS_SET_KEY, programmer);
+	}
+
+	@Override
+	public void saveHash(Programmer programmer) {
+		redisTemplate.opsForHash().put(REDIS_HASH_KEY, programmer.getId(), programmer);
+		
+	}
+
+	@Override
+	public void updateHash(Programmer programmer) {
+		redisTemplate.opsForHash().put(REDIS_HASH_KEY, programmer.getId(), programmer);		
+	}
+
+	@Override
+	public Map<Integer, Programmer> findAllHash() {
+		return (Map<Integer, Programmer>) (Map<?, ?>) redisTemplate.opsForHash().entries(REDIS_HASH_KEY);
+	}
+
+	@Override
+	public Programmer findInHash(int id) {
+		return (Programmer) redisTemplate.opsForHash().get(REDIS_HASH_KEY, id);
+	}
+
+	@Override
+	public void deleteHash(int id) {
+		redisTemplate.opsForHash().delete(REDIS_HASH_KEY, id);
+	}
+	
+	
 	
 }
--- SpringBootJedisDemo1/src/main/java/com/example/jedis/SpringBootJedisDemo1/dao/ProgrammerRepository.java
+++ SpringBootJedisDemo1/src/main/java/com/example/jedis/SpringBootJedisDemo1/dao/ProgrammerRepository.java
@@ -1,8 +1,32 @@
 package com.example.jedis.SpringBootJedisDemo1.dao;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.example.jedis.SpringBootJedisDemo1.model.Programmer;
+
 public interface ProgrammerRepository {
 
+	// String => Key - Value pair
 	void setProgrammerAsString(String idKey, String programmer);
 	String getProgrammerAsString(String idKey);
+	
+	// List
+	void addToProgrammerList(Programmer programmer);
+	List<Programmer> getProgrammerListMembers();
+	Long getProgrammersListCount();
+	
+	// Set
+	void AddProgrammerToSet(Programmer ...programmer);
+	Set<Programmer> getProgrammersSet();
+	boolean isSetMember(Programmer programmer);
+	
+	// Hash
+	void saveHash(Programmer programmer);
+	void updateHash(Programmer programmer);
+	Map<Integer, Programmer> findAllHash();
+	Programmer findInHash(int id);
+	void deleteHash(int id);
 	
 }
